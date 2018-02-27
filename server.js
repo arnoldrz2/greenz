b6187837494bb064b86c9676f75b13cb40bfda57
arnoldrz2 <arnoldrz2@gmail.com>
2018-02-26 19:39:28 -0600

    modified server js to work with orm/handlebars and added controllers and  folders

--- server.js
+++ server.js
@@ -1,19 +1,34 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-var app  = express();
+var methodOverride = require("method-override");
 var exphbs = require("express-handlebars");
 
 var PORT = process.env.PORT || 8080;
 
-app.use(bodyParser.json());
+var app  = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json, text, api json(?)
+app.use(bodyParser.json());
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+// Set Handlebars.
+var exphbs = require("express-handlebars");
 
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
+// Import routes and give the server access to them.
+// var routes = require("./controllers/*.js"); this is commented out because controller hasn't been setup yet
+
+// app.use(routes); this is commmented out because routes haven't been setup yet
+
 app.listen(PORT, function(){
-    console.log("I am on the port " +PORT);
+    console.log("I am on port " + PORT);
 });
