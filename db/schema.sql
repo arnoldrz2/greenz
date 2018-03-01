af224ffd931a0888fd819127a34782560c45c432
Arnold R Zimmerman Jr <arnoldrz2@gmail.com>
2018-03-01 15:08:21 -0600

    took out user birth date since we aren't going to use that data for now

--- db/schema.sql
+++ db/schema.sql
@@ -6,7 +6,6 @@ CREATE TABLE users
 	id INT NOT NULL AUTO_INCREMENT,
 	user_name VARCHAR(255) NOT NULL,
 	user_email VARCHAR(255),
-	user_birth DATE,
 	user_favorite_strain VARCHAR(255),
 	PRIMARY KEY (id)
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,5 @@
-INSERT INTO users (user_name, user_email, user_birth, user_favorite_strain) VALUES ('Ivan', 'ivanmorenoz47@gmail.com', '1979-08-18', 'Acapulco Gold');
+INSERT INTO users (user_name, user_email, user_favorite_strain) VALUES ('Ivan', 'ivanmorenoz47@gmail.com', 'Acapulco Gold');
 
-INSERT INTO users (user_name, user_email, user_birth, user_favorite_strain) VALUES ('Jesse', 'jesseflores512@gmail.com', '1989-04-15', 'Khalifa Kush');
+INSERT INTO users (user_name, user_email, user_favorite_strain) VALUES ('Jesse', 'jesseflores512@gmail.com', 'Khalifa Kush');
 
-INSERT INTO users (user_name, user_email, user_birth, user_favorite_strain) VALUES ('Arnold', 'arnoldrz2@gmail.com', '1988-01-03', 'Skywalker');
+INSERT INTO users (user_name, user_email, user_favorite_strain) VALUES ('Arnold', 'arnoldrz2@gmail.com', 'Skywalker');
