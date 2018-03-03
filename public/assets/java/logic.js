dd326bc1c8c911df92c075a0d3269d5da6f7a63f
jesseflores512 <jesseflores512@gmail.com>
2018-03-02 22:00:23 -0600

    modified layouts

--- public/assets/java/logic.js
+++ public/assets/java/logic.js
@@ -1,6 +1,25 @@
 // API Key
-var authKey = "xBCEImO";
+var apikey = "ececee688d4f53e59ea89f04c7943e17d5e2d3bf";
 
 $(document).ready(function(){
     console.log("ready!");
+
+    $("#submit").on("click", function(event) {
+  event.preventDefault();
+
+  var queryURL = "https://api.otreeba.com/v1/strains?page=1&count=10&sort=-createdAt"
+
+        $.ajax({
+          url: queryURL,
+          method: "GET"
+        })
+
+        .done(function(response) {
+        // var rep = response.data;
+         console.log(response);
+       });
+
+
+
+
 })
